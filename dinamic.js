a728cea5128eaeb0c9105ef47c7efb0989688ee3
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-08-24 17:39:41 -0300

    Agregamos javascript

--- dinamic.js
+++ dinamic.js
@@ -13,5 +13,19 @@ function mostrar(){
           "\ntu Fecha de Nacimiento es: " + fechaNacimiento);
 }
 
+function consultar(){
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+
+    if(edad >= 18){
+        alert(`Hola ${nombre} sos mayor de edad y podes ver el sitio tranquilamente.`);
+    }else{
+        alert(`Hola ${nombre}, por favor NO sos mayor de edad`)
+    }
+}
+
+
+
+
 
 
