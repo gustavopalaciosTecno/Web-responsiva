f802db7600fe534f71f16650b89c361a1a552b03
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-08-24 17:19:43 -0300

    Agregamos javascript

--- dinamic.js
+++ dinamic.js
@@ -1,3 +1,17 @@
 function activarBoton(){
     alert("Hiciste click en el botón");
 }
+
+function mostrar(){
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let dni = document.getElementById("documento").value;
+    let fechaNacimiento = document.getElementById("fechaNacimeinto").value;
+
+    alert("Hola, " + nombre + " " + apellido + 
+          "\nDNI: " + dni + 
+          "\nFecha de Nacimiento: " + fechaNacimiento);
+}
+
+
+
